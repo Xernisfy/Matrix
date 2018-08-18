3565538210eea3221c55762122c9192813d3307b
Xernisfy <xernisfy@gmail.com>
2018-08-19 00:27:27 +0200

    Test ghostImage 2

--- js/Character.js
+++ js/Character.js
@@ -28,6 +28,7 @@ class Character {
     this.c.shadowColor = '#00000000';
     if (g && (config.ghostText || config.special === 'time')) {
       let imgData = g.getImageData(this.x, this.y - (this.line.size / 2), 1, 1).data;
+      console.log(imgData);
       d.putImageData(d.createImageData(this.line.size, this.line.size), this.x - this.line.size / 2, this.y - this.line.size);
       if (imgData[3] !== 0 && !this.line.erase) {
         d.fillStyle = mapColor(config.ghostColor);
